e5b87ae5d7e6d4c19eb3d8c91bac57e016e87e60
Aayush3cluz <37142737+Aayush3cluz@users.noreply.github.com>
2022-05-24 14:06:39 +1000

    Hotfix - made publishDates AEST formatted

--- src/use-cases/fetch-and-map-feed.js
+++ src/use-cases/fetch-and-map-feed.js
@@ -1,24 +1,29 @@
 const getRSSFeed = require("../utilities/rss-parser");
-
+const {
+  convertISODateToAEST,
+} = require("../utilities/format-iso-date-to-AEST");
 const fetchAndCustomizeRssFeed = async (url, sort = false, order = "asc") => {
   try {
     let feedData = await getRSSFeed(url);
 
-    let episodes = feedData.items.slice(0, 10).map((item) => {
+    let episodes = feedData.items.slice(0, 10);
+
+    if (sort) {
+      console.log(order);
+      episodes = episodes.sort((a, b) => {
+        return order === "asc"
+          ? new Date(a.pubDate) - new Date(b.pubDate)
+          : new Date(b.pubDate) - new Date(a.pubDate);
+      });
+    }
+
+    episodes = episodes.map((item) => {
       return {
         title: item.title,
         audioUrl: item.enclosure.url,
-        publishDate: item.pubDate,
+        publishDate: convertISODateToAEST(item.pubDate),
       };
     });
-
-    if (sort) {
-      episodes = episodes.sort((a, b) => {
-        return order === "asc"
-          ? new Date(a.publishDate) - new Date(b.publishDate)
-          : new Date(b.publishDate) - new Date(a.publishDate);
-      });
-    }
 
     return {
       title: feedData.title,
